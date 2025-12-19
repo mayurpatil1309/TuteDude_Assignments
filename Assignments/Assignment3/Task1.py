54f8942a095cae27a18c4945b114087161420532
mayurpatil1309 <mayurpatil1309@gmail.com>
2025-12-19 20:48:28 +0530

    Assignment3

---
+++ Assignments/Assignment3/Task1.py
@@ -0,0 +1,23 @@
+"""
+Task 1: Calculate Factorial Using a Function
+
+
+Problem Statement: Write a Python program that:
+1.   Defines a function named factorial that takes a number as an argument and calculates its factorial using a loop or recursion.
+2.   Returns the calculated factorial.
+3.   Calls the function with a sample number and prints the output.
+
+
+"""
+
+def factorial(number):
+    if number == 0:
+        return 1
+    else:
+        return number * factorial(number - 1)
+
+num= int(input("Enter a number: "))
+print(f"Factorial of {num} is : {factorial(num)}")
+
+
+
---
+++ Assignments/Assignment3/Task2.py
@@ -0,0 +1,22 @@
+"""
+Task 2: Using the Math Module for Calculations
+
+Problem Statement: Write a Python program that:
+1.   Asks the user for a number as input.
+2.   Uses the math module to calculate the:
+o   Square root of the number
+o   Natural logarithm (log base e) of the number
+o   Sine of the number (in radians)
+3.   Displays the calculated results.
+
+"""
+import math
+number = int(input("Enter a number: "))
+
+square_root = math.sqrt(number)
+logarithm = math.log(number)
+sine = math.sin(number)
+
+print(f"Square root : {square_root}")
+print(f"Logarithm : {logarithm}")
+print(f"Sine : {sine}")
